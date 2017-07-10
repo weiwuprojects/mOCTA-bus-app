9f62561e6e20a61a173906cf854513cc54a14069
Andrew Chan <chana9@uci.edu>
2017-07-09 20:48:15 -0700

    date field now saves changes

--- app/src/main/java/wuweiprojects/com/moctabusapp/MainActivity.java
+++ app/src/main/java/wuweiprojects/com/moctabusapp/MainActivity.java
@@ -1,23 +1,28 @@
 package wuweiprojects.com.moctabusapp;
 
-import android.app.ActionBar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
-import android.widget.TextView;
+
 import android.widget.VideoView;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
     VideoView vv;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -37,29 +42,50 @@ public class MainActivity extends AppCompatActivity {
         final EditText expDate = (EditText) findViewById(R.id.textView3);
         expDate.setCursorVisible(false);
         expDate.getBackground().setColorFilter(0xff1a5eab, PorterDuff.Mode.SRC_IN);
+        SharedPreferences prefs = getSharedPreferences("textField", MODE_PRIVATE);
+        String text = prefs.getString("date", "Aug 10, 2017 3:00 PM");
+        expDate.setText(text);
 
+        expDate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                SharedPreferences.Editor editor = getSharedPreferences("textField", MODE_PRIVATE).edit();
+                editor.putString("date", expDate.getText().toString());
+                editor.apply();
+            }
+        });
+
+        //video stuff
         vv = (VideoView) findViewById(R.id.videoView);
         String path = "android.resource://" + getPackageName() + "/" + R.raw.video;
         vv.setVideoURI(Uri.parse(path));
-
         vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 mp.setLooping(true);
             }
         });
-
         vv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 vv.requestFocus();
             }
         });
-
         vv.start();
 
 
     }
+
 
 
 }
