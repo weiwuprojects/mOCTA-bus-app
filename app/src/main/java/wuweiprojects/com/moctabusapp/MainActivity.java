d3c107e80c1acddcccb5f7be12f53508465982a3
Andrew Chan <chana9@uci.edu>
2017-07-09 20:14:48 -0700

    initial release

---
+++ app/src/main/java/wuweiprojects/com/moctabusapp/MainActivity.java
@@ -0,0 +1,66 @@
+package wuweiprojects.com.moctabusapp;
+
+import android.app.ActionBar;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView vv;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(0xff1a5eab);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar4);
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+
+        final EditText expDate = (EditText) findViewById(R.id.textView3);
+        expDate.setCursorVisible(false);
+        expDate.getBackground().setColorFilter(0xff1a5eab, PorterDuff.Mode.SRC_IN);
+
+        vv = (VideoView) findViewById(R.id.videoView);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        vv.setVideoURI(Uri.parse(path));
+
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+        vv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vv.requestFocus();
+            }
+        });
+
+        vv.start();
+
+
+    }
+
+
+}
+
